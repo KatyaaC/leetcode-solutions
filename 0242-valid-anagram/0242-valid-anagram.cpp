953c66769dd908e8cb39553460e97f1f48e08028
Katya Chadha <katya.official27@gmail.com>
2026-06-24 21:12:04 +0530

    Time: 0 ms (100%), Space: 9.9 MB (11.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if (s.size()!=t.size()){
+            return false;
+        }
         unordered_map<char,int> mp;
-        unordered_map<char,int> mp2;
         for (char x: s){
             mp[x]++;
         }
-        for (char j: t){
-            mp2[j]++;
-            
+        for (char k : t){
+            mp[k]--;
+            if (mp[k]<0){
+                return false;
+            }
         }
-        if (mp==mp2){
-            return true;
-        }
-        return false;
-        
+        return true;
     }
 };
