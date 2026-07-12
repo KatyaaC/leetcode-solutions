d8cf15d16f08b828c83153c9a22cfdd43ee30bf6
Katya Chadha <katya.official27@gmail.com>
2026-07-12 20:32:08 +0530

    Time: 0 ms (100%), Space: 9 MB (38.18%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <char> st;
+        for (char c:s){
+            if (c=='{' or c=='[' or c=='('){
+                st.push(c);
+            }
+            else{
+            if (st.empty()){return false;}
+            if (c==')' and st.top()!='('){
+                return false;
+            }
+            else if (c==']' and st.top()!='['){
+                return false;
+            }
+            else if (c=='}' and st.top()!='{'){
+                return false;
+            }
+            st.pop();
+
+        }}
+        return st.empty();
+    }
+};
